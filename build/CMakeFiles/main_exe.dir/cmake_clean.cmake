eb013226798ba47af8dc839e92b9e8a792c7b447
ngnepist <stephane.ngnepiepaye@ensta-bretagne.org>
2020-11-09 12:32:43 +0100

    avec le makedir

---
+++ build/CMakeFiles/main_exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_exe.dir/src/Cell.cpp.o"
+  "CMakeFiles/main_exe.dir/src/Path.cpp.o"
+  "CMakeFiles/main_exe.dir/src/main.cpp.o"
+  "CMakeFiles/main_exe.dir/src/vibes.cpp.o"
+  "main_exe.pdb"
+  "main_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
